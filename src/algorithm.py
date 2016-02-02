0831ca1eaa3dcf98bbc5a6097be1091fe8675f07
Noam Av <copythegolden@gmail.com>
2016-02-02 23:54:25 +0200

    fixed rotate with Hold key (10/120sec refresh delay)

--- src/algorithm.py
+++ src/algorithm.py
@@ -3,6 +3,7 @@ import pygame
 import graphics
 import classes
 class pyAlgorithm:
+    __lastTimeSinceKey={"Left":0,"Right":0}
     __kinput=[]
     myCell=None
     __cellList=[]
@@ -12,12 +13,29 @@ class pyAlgorithm:
         self.myCell=classes.baseCell(0,0,100,100,1,5,1000,classes.Location(100,100),10)
     def getInput(self):
         returnList=[]
-        for event in pygame.event.get(pygame.KEYDOWN):
+        events = pygame.event.get(pygame.KEYDOWN)
+        for event in events:
             if event.key==pygame.K_LEFT:
                 returnList.append("Left")
+                self.__lastTimeSinceKey["Left"]=0
             if event.key==pygame.K_RIGHT:
                 returnList.append("Right")
+                self.__lastTimeSinceKey["Right"]=0
         key=pygame.key.get_pressed()
+        #check for hold
+        if key[pygame.K_LEFT]:
+            if self.__lastTimeSinceKey["Left"]>10:
+                returnList.append("OLeft")
+                self.__lastTimeSinceKey["Left"]=0
+            else:
+                self.__lastTimeSinceKey["Left"]+=1
+        if key[pygame.K_RIGHT]:
+            if self.__lastTimeSinceKey["Right"]>10:
+                returnList.append("ORight")
+                self.__lastTimeSinceKey["Right"]=0
+            else:
+                self.__lastTimeSinceKey["Right"]+=1
+        #check up and down
         if key[pygame.K_UP]:
             returnList.append("Up")
         if key[pygame.K_DOWN]:
@@ -31,9 +49,9 @@ class pyAlgorithm:
             else:
                 self.__kinput.append(item)
         for item in self.__kinput:
-            if item=="Left":
+            if item=="Left" or item=="OLeft":
                 self.myCell.changeAngle(-1)
-            if item=="Right":
+            if item=="Right" or item=="ORight":
                 self.myCell.changeAngle(1)
         if "Up" in self.__kinput:
             self.myCell.move()
