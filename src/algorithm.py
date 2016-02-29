8199a19f426b5de0eb27d6757f86c37ae1130a0a
omri1100 <omri1100@gmail.com>
2016-02-29 16:08:46 +0200

    modified vision

--- src/algorithm.py
+++ src/algorithm.py
@@ -21,7 +21,7 @@ class pyAlgorithm:
     foodList=[]
     def __init__(self): #temp
         self.myCell=classes.baseCell(None,0,0,200,100,360,0,100,classes.Location(100,100),3,10,600,10,0,1,3)
-        self.cellList.append(classes.baseCell(None,0,0,100,100,360,1,1,classes.Location(200,200),3,10,1200,15,1,1,3))
+        self.cellList.append(classes.baseCell(None,0,0,100,100,360,1,1,classes.Location(200,200),3,10,1200,15,2,10000,3))
         self.cellEggs.append(self.cellList[0].layEgg())
         ##self.cellEggs.append(classes.Egg((100,100),self.cellList[0],None,6,0))
         for i in xrange(10):
